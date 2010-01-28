a4c071cf723f1c44a23722e413650c91275c7216
holborn <holborn@users.sourceforge.net>
2010-01-28 16:04:33 +0000

    new file:   Ring.C 	new file:   Ring.h

---
+++ src/Ring.C
@@ -0,0 +1,347 @@
+/*
+  Rakarrack Audio FX
+  Valve DSP Code based Steve Harris valve LADSPA plugin(swh-plugins).
+  ZynAddSubFX effect structure - Copyright (C) 2002-2005 Nasca Octavian Paul
+  Modified and adapted for rakarrack by Josep Andreu
+
+  Valve.C - Distorsion effect
+  
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License 
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License (version 2) for more details.
+
+  You should have received a copy of the GNU General Public License (version 2)
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "Ring.h"
+
+
+
+
+Ring::Ring (REALTYPE * efxoutl_, REALTYPE * efxoutr_)
+{
+
+  efxoutl = efxoutl_;
+  efxoutr = efxoutr_;
+
+  sin_tbl = (float *) malloc(sizeof(float) * SAMPLE_RATE);
+  tri_tbl = (float *) malloc(sizeof(float) * SAMPLE_RATE);
+  squ_tbl = (float *) malloc(sizeof(float) * SAMPLE_RATE);
+  saw_tbl = (float *) malloc(sizeof(float) * SAMPLE_RATE);
+
+  Create_Tables();
+
+  offset = 0;
+
+
+  //default values
+  Ppreset = 0;
+  Pvolume = 50;
+  Plrcross = 40;
+  Plevel = 64;
+  Pstereo = 0;
+
+  scale = 1.0f;
+  sin = 0.0f;
+  tri = 0.0f;
+  saw = 0.0f;
+  squ = 0.0f;
+
+  setpreset (Ppreset);
+  cleanup ();
+};
+
+Ring::~Ring ()
+{
+ free(sin_tbl);
+ free(tri_tbl);
+ free(squ_tbl);
+ free(saw_tbl);
+
+};
+
+/*
+* Create Tables
+*/
+void
+Ring::Create_Tables()
+{
+unsigned int i;
+float SR = (float) SAMPLE_RATE;  
+  
+
+  for (i=0;i<SAMPLE_RATE;i++) sin_tbl[i]=sinf((float)i*D_PI/SR);
+  for (i=0;i<SAMPLE_RATE;i++) tri_tbl[i]=acosf(cosf((float)i*D_PI/SR))/D_PI-1.0f;
+  for (i=0;i<SAMPLE_RATE;i++) squ_tbl[i]=(i < SAMPLE_RATE/2) ? 1.0f : -1.0f;
+  for (i=0;i<SAMPLE_RATE;i++) saw_tbl[i]=((2.0f*i)-SR)/SR;
+
+}
+
+
+
+/*
+ * Cleanup the effect
+ */
+void
+Ring::cleanup ()
+{
+
+
+};
+
+
+/*
+ * Apply the filters
+ */
+
+
+/*
+ * Effect output
+ */
+void
+Ring::out (REALTYPE * smpsl, REALTYPE * smpsr)
+{
+  int i;
+  REALTYPE l, r, lout, rout;
+
+  REALTYPE inputvol = powf (5.0f, ((float)Pinput - 32.0f) / 127.0f);
+
+  if (Pstereo != 0)
+    {				//Stereo
+      for (i = 0; i < PERIOD; i++)
+	{
+	  efxoutl[i] = smpsl[i] * inputvol * 2.0f;
+	  efxoutr[i] = smpsr[i] * inputvol * 2.0f;
+	};
+    }
+  else
+    {
+      for (i = 0; i < PERIOD; i++)
+	{
+	  efxoutl[i] =
+	    (smpsl[i]  +  smpsr[i] ) * inputvol;
+	};
+    };
+
+
+
+  for (i=0;i < PERIOD; i++)
+    {
+
+     efxoutl[i] *= ( depth * ((( sin * scale) * sin_tbl[offset] + ((tri * scale) * tri_tbl[offset]) + ((saw * scale) * saw_tbl[offset]) + ((squ * scale) * squ_tbl[offset])) + ( 1.0f - depth)));
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+  if (Pstereo != 0)
+     {
+      efxoutr[i] *= ( depth * ((( sin * scale) * sin_tbl[offset] + ((tri * scale) * tri_tbl[offset]) + ((saw * scale) * saw_tbl[offset]) + ((squ * scale) * squ_tbl[offset])) + ( 1.0f - depth)));
+     }
+
+      offset += Pfreq;
+      if (offset > SAMPLE_RATE) offset -=SAMPLE_RATE;
+  }
+
+
+  if (Pstereo == 0) memcpy (efxoutr , efxoutl, PERIOD * sizeof(float));
+
+  REALTYPE level = dB2rap (60.0f * (float)Plevel / 127.0f - 40.0f);
+
+  for (i = 0; i < PERIOD; i++)
+    {
+      lout = efxoutl[i];
+      rout = efxoutr[i];
+        
+      
+      l = lout * (1.0f - lrcross) + rout * lrcross;
+      r = rout * (1.0f - lrcross) + lout * lrcross;
+
+      lout = l;
+      rout = r;  
+       
+      efxoutl[i] = lout * 2.0f * level * panning;
+      efxoutr[i] = rout * 2.0f * level * (1.0f -panning);  
+
+    };
+    
+};
+
+
+/*
+ * Parameter control
+ */
+void
+Ring::setvolume (int Pvolume)
+{
+  this->Pvolume = Pvolume;
+
+  outvolume = (float)Pvolume / 127.0f;
+  if (Pvolume == 0)
+    cleanup ();
+
+};
+
+void
+Ring::setpanning (int Ppanning)
+{
+  this->Ppanning = Ppanning;
+  panning = ((float)Ppanning + 0.5f) / 127.0f;
+};
+
+
+void
+Ring::setlrcross (int Plrcross)
+{
+  this->Plrcross = Plrcross;
+  lrcross = (float)Plrcross / 127.0f * 1.0f;
+};
+
+
+void
+Ring::setscale()
+{
+  scale = sin + tri + saw + squ;
+  if (scale==0.0) scale = 1.0;
+  scale = 1.0 / scale;
+}
+
+void
+Ring::setpreset (int npreset)
+{
+  const int PRESET_SIZE = 11;
+  const int NUM_PRESETS = 6;
+  int presets[NUM_PRESETS][PRESET_SIZE] = {
+    //Ring 1
+    {0, 64, 64, 64, 64, 0, 127, 0, 0, 0, 0},
+    //Rong 2
+    {0, 64, 35, 29, 45, 1, 0, 127, 0, 0, 0},
+    //Ring 3
+    {0, 64, 0, 87, 14, 6, 0, 80, 30, 0, 1},
+    //Ring 4
+    {0, 64, 127, 87, 14, 0, 1, 80, 24, 0, 0},
+    //Ring 5
+    {0, 64, 127, 127, 12, 13, 0, 90, 16, 0, 1},
+    //Ring Amp
+    {84, 64, 35, 63, 50, 2, 0, 55, 0, 0, 0}
+  };
+
+
+  if (npreset >= NUM_PRESETS)
+    npreset = NUM_PRESETS - 1;
+  for (int n = 0; n < PRESET_SIZE; n++)
+    changepar (n, presets[npreset][n]);
+  Ppreset = npreset;
+  cleanup ();
+};
+
+
+void
+Ring::changepar (int npar, int value)
+{
+  switch (npar)
+    {
+    case 0:
+      setvolume (value);
+      break;
+    case 1:
+      setpanning (value);
+      break;
+    case 2:
+      setlrcross (value);
+      break;
+    case 3:
+      Plevel = value;
+      break;
+    case 4:
+      Pdepthp = value;
+      depth = (float) Pdepthp / 100.0;
+      break;
+    case 5:
+      Pfreq = value;
+      break;
+    case 6:
+      if (value > 1)
+	value = 1;
+      Pstereo = value;
+      break;
+    case 7:
+      Psin = value;
+      sin = (float) Psin / 100.0;
+      setscale();
+      break;
+    case 8:
+      Ptri = value;
+      tri = (float) Ptri / 100.0;
+      setscale();
+      break;       
+    case 9:
+      Psaw = value;
+      saw = (float) Psaw / 100.0;
+      setscale();
+      break;
+    case 10:
+      Psqu = value;
+      squ = (float) Psqu / 100.0;
+      setscale();
+      break;       
+    case 11:
+      Pinput = value;
+      break;
+    };
+};
+
+int
+Ring::getpar (int npar)
+{
+  switch (npar)
+    {
+    case 0:
+      return (Pvolume);
+      break;
+    case 1:
+      return (Ppanning);
+      break;
+    case 2:
+      return (Plrcross);
+      break;
+    case 3:
+      return (Plevel);
+      break;
+    case 4:
+      return (Pdepthp);
+      break;
+    case 5:
+      return (Pfreq);
+      break;
+    case 6:
+      return (Pstereo);
+      break;
+    case 7:
+      return (Psin);
+      break;
+    case 8:
+      return (Ptri);
+      break;  
+    case 9:
+      return (Psaw);
+      break;
+    case 10:
+      return (Psqu);
+      break;  
+    case 11:
+      return (Pinput);
+      break;
+
+
+    };
+  return (0);			//in case of bogus parameter number
+};
+
