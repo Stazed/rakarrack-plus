bc0a78a729a9b128c5c2c991b9b42f5e6fa470bd
holborn <holborn@users.sourceforge.net>
2010-02-10 15:28:02 +0000

    modified:   process.C

--- src/process.C
+++ src/process.C
@@ -295,10 +295,10 @@ RKR::init_rkr ()
 
 efx_FLimiter->Compressor_Change(1,-3);
 efx_FLimiter->Compressor_Change(2,15);
-efx_FLimiter->Compressor_Change(3,0);
+efx_FLimiter->Compressor_Change(3,-4);
 efx_FLimiter->Compressor_Change(4,10);
 efx_FLimiter->Compressor_Change(5,500);
-efx_FLimiter->Compressor_Change(6,0);
+efx_FLimiter->Compressor_Change(6,1);
 efx_FLimiter->Compressor_Change(7,0);
 efx_FLimiter->Compressor_Change(8,1);
 
