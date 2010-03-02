7e10be0a55b1994d2299724829d964b09fe14b02
holborn <holborn@users.sourceforge.net>
2010-03-02 08:00:18 +0000

    modified:   Convolotron.C

--- src/Convolotron.C
+++ src/Convolotron.C
@@ -221,8 +221,9 @@ Convolotron::process_rbuf()
  
 	 }
 	 
-//	 if(maxamp < 0.003f) maxamp = 0.003f;
+	 if(maxamp < 0.3f) maxamp = 0.3f;
 	 ngain = IRpowa/(IRpowb*maxamp);
+	 if (ngain > maxx_read) ngain = maxx_read;
 	 for(j=0; j<length; j++) buf[j] *= ngain; 
 	 printf("maxamp %f  IRpowa %f  IRpowb %f ngain %f maxx_read %d length %d\n", maxamp, IRpowa, IRpowb, ngain, maxx_read, length);
 
