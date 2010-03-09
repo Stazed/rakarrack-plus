12107eb939a6e9a8fde5bd0f1463bdd83cabfb62
Ryan Billing <ryjobil@gmail.com>
2010-03-08 17:09:22 -0900

    RyanWah header 	modified:   src/RyanWah.h

--- src/RyanWah.h
+++ src/RyanWah.h
@@ -53,12 +53,23 @@ public:
 private:
   //Parametrii RyanWah
     EffectLFO lfo;		//lfo-ul RyanWah
-  int Pvolume;
-  int Ppanning;
-  int Pdepth;		//the depth of the lfo of the RyanWah
-  int Pampsns;	//how the filter varies according to the input amplitude
-  int Pampsnsinv;	//if the filter freq is lowered if the input amplitude rises
-  int Pampsmooth;	//how smooth the input amplitude changes the filter
+    //GUI include:
+    //Tempo
+    //Type
+          //if space is needed, remove st. diff and randomness.
+	  
+  int Pvolume;		//For wet/dry mix
+  //int Ppanning;        //delete.  
+  int Pwidth;		//0/127// The width of the lfo of the RyanWah
+  int Pampsns;	        //0/127//how the filter varies according to the input amplitude
+  int Pampsnsinv;	//-64/64//bias of the filter
+  int Pampsmooth;	//0/127//how smooth the input amplitude changes the filter, also for Pampsinsinv smoothing for wah wah pedal
+  int Prange;		//10/5000Hz//how far filter can deviate.
+  int Php		//-64/64//high pass filter mix
+  int Plp;		//-64/64//low pass filter mix
+  int Pbp;		//-64/64//Bandpass filter mix
+  int Pq;		//0/127//filter resonance.
+  
 
   //Control Parametrii
   void setvolume (int Pvolume);
