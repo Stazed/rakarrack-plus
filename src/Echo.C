921c82c5500af43577d7a643dfc29d817de968e8
holborn <holborn@users.sourceforge.net>
2010-05-02 15:36:03 +0100

    modified:   ChangeLog 	modified:   src/Echo.C

--- src/Echo.C
+++ src/Echo.C
@@ -7,6 +7,7 @@
 
   Modified for rakarrack by Josep Andreu
   Reverse Echo effect by Transmogrifox
+  Echo Direct patch From: Arnout Engelen <arnouten@bzzt.net>
   
   This program is free software; you can redistribute it and/or modify
   it under the terms of version 2 of the GNU General Public License 
