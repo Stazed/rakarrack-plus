04280429397edf8c22709b8a9ba888db5baf4a71
Ryan Billing <ryjobil@gmail.com>
2010-07-23 17:34:25 -0800

    line change 	modified:   src/Vibe.C

--- src/Vibe.C
+++ src/Vibe.C
@@ -106,11 +106,8 @@ Vibe::out (float *smpsl, float *smpsr)
   else if (lfor < 0.0f)
     lfor = 0.0f;  
     
-    //lfor = powf(lfor, 1.9f);   // 
-    //lfol = powf(lfol, 1.9f);  //emulate lamp turn on/off characteristic by experimentation
-
     lfor = 2.0f - 2.0f/(lfor + 1.0f);   // 
-    lfol = 2.0f - 2.0f/(lfol + 1.0f); //emulate lamp turn on/off characteristic by experimentation 
+    lfol = 2.0f - 2.0f/(lfol + 1.0f); //emulate lamp turn on/off characteristic by typical curves 
       
   for (i = 0; i < PERIOD; i++)
     {
