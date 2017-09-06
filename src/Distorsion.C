2ef77fec07a79cf7801eae945c8c7e5c43e8a8a6
stazed <stazed@KudoMapson.com>
2017-09-06 14:23:17 -0500

    Fix Distorsion PRESET_SIZE & fix presets.

--- src/Distorsion.C
+++ src/Distorsion.C
@@ -319,22 +319,22 @@ Distorsion::setoctave (int Poctave)
 void
 Distorsion::setpreset (int dgui, int npreset)
 {
-    const int PRESET_SIZE = 11;
+    const int PRESET_SIZE = 13;
     const int NUM_PRESETS = 6;
     int pdata[MAX_PDATA_SIZE];
     int presets[NUM_PRESETS][PRESET_SIZE] = {
         //Overdrive 1
-        {84, 64, 35, 56, 40, 0, 0, 6703, 21, 0, 0},
+        {84, 64, 35, 56, 40, 0, 0, 6703, 21, 0, 0, 0, 0},
         //Overdrive 2
-        {85, 64, 35, 29, 45, 1, 0, 25040, 21, 0, 0},
+        {85, 64, 35, 29, 45, 1, 0, 25040, 21, 0, 0, 0 , 0},
         //Distorsion 1
-        {0, 64, 0, 87, 14, 6, 0, 3134, 157, 0, 1},
+        {0, 64, 0, 87, 14, 6, 0, 3134, 157, 0, 1, 0, 0},
         //Distorsion 2
-        {0, 64, 127, 87, 14, 0, 1, 3134, 102, 0, 0},
+        {0, 64, 127, 87, 14, 0, 1, 3134, 102, 0, 0, 0, 20},
         //Distorsion 3
-        {0, 64, 127, 127, 12, 13, 0, 5078, 56, 0, 1},
+        {0, 64, 127, 127, 12, 13, 0, 5078, 56, 0, 1, 0, 0},
         //Guitar Amp
-        {84, 64, 35, 63, 50, 2, 0, 824, 21, 0, 0}
+        {84, 64, 35, 63, 50, 2, 0, 824, 21, 0, 0, 0 , 0}
     };
 
 
