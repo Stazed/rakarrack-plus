e80507b53603ce51b0481a7db99d374d397ca61d
stazed <stazed@KudoMapson.com>
2017-09-09 14:45:18 -0500

    Cleanup.

--- src/NewDist.C
+++ src/NewDist.C
@@ -266,8 +266,6 @@ NewDist::out (float * efxoutl, float * efxoutr)
             rout = r;
         }
 
-        //efxoutl[i] = lout * level * panning;
-        //efxoutr[i] = rout * level * ( 1.0f - panning);
         efxoutl[i] = lout * level * ( 1.0f - panning);
         efxoutr[i] = rout * level * panning;
 
