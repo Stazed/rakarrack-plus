b9319e44651f9d669f371160d5aa4fbe6c9a1db0
stazed <stazed@KudoMapson.com>
2017-09-09 09:09:28 -0500

    Fix WahWah user preset loading.

--- src/DynamicFilter.C
+++ src/DynamicFilter.C
@@ -195,7 +195,7 @@ DynamicFilter::setpreset (int npreset)
 
     if(npreset>NUM_PRESETS-1) {
 
-        Fpre->ReadPreset(11,npreset-NUM_PRESETS+1,pdata);
+        Fpre->ReadPreset(10,npreset-NUM_PRESETS+1,pdata);
         for (int n = 0; n < PRESET_SIZE; n++)
             changepar (n, pdata[n]);
     } else {
