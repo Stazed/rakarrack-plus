3d337d7f12bf3fafe095bb6306ba08da1e8cbefa
stazed <stazed@KudoMapson.com>
2017-09-15 08:50:16 -0500

    Expand MAX_PRESET_SIZE to allow for additional parameters of RyanWah.C (MUTroMojo). Fix default preset loading, off by one for additional parameter #10 of order.

--- src/global.h
+++ src/global.h
@@ -87,7 +87,7 @@
 #define TEMPBUFSIZE 1024
 #define MAX_PDATA_SIZE 50
 #define USERFILE 100    // used by Convolotron, Echotron, Reverbtron to indicate user file in setfile()
-#define MAX_PRESET_SIZE 16  // fileio.C presets_default[][]
+#define MAX_PRESET_SIZE 19  // fileio.C presets_default[][]
 
 typedef union {
     float f;
