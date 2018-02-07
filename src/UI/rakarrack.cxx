eb47a70e82436db916247d254353a8e5e9afa629
Stazed <stazed@manjo-mapson.com>
2018-02-07 15:07:32 -0600

    Move vocoder to separate file.

--- src/UI/rakarrack.cxx
+++ src/UI/rakarrack.cxx
@@ -558,8 +558,8 @@ rkr->efx_FLimiter->limit=0;
 
 if(rkr->checkforaux())
 {
- if(vu_vu->value()!=rkr->efx_Vocoder->vulevel)
- vu_vu->value(rkr->efx_Vocoder->vulevel);
+ if(VOCODER->vu_vu->value()!=rkr->efx_Vocoder->vulevel)
+ VOCODER->vu_vu->value(rkr->efx_Vocoder->vulevel);
 }
 
 if (Sco->get_scope_ON())
@@ -749,129 +749,6 @@ Fl::focus(Open_Order);
 }
 void RKRGUI::cb_TITTLE_L(Fl_Button* o, void* v) {
   ((RKRGUI*)(o->parent()->user_data()))->cb_TITTLE_L_i(o,v);
-}
-
-void RKRGUI::cb_vo_activar_i(Fl_Light_Button* o, void*) {
-  if(Fl::event_button()==3)
-{
- getMIDIControl(116);
- o->value(rkr->Vocoder_Bypass);
- return;
-}
-rkr->Vocoder_Bypass=(int)o->value();
-if((int) o->value()==0)
-vu_vu->value(-48);
-findpos(35,(int)o->value(),o);
-}
-void RKRGUI::cb_vo_activar(Fl_Light_Button* o, void* v) {
-  ((RKRGUI*)(o->parent()->parent()->user_data()))->cb_vo_activar_i(o,v);
-}
-
-void RKRGUI::cb_vo_preset_i(Fl_Choice* o, void* v) {
-  long long ud= (long long) v;
-        if((ud==0)||(ud==12035))rkr->efx_Vocoder->setpreset((int) o->value());
-        vo_WD->value(rkr->efx_Vocoder->getpar(0)-64);
-        vo_pan->value(rkr->efx_Vocoder->getpar(1)-64);
-        vo_mu->value(rkr->efx_Vocoder->getpar(2));
-        vo_q->value(rkr->efx_Vocoder->getpar(3));
-        vo_input->value(rkr->efx_Vocoder->getpar(4));
-        vo_ring->value(rkr->efx_Vocoder->getpar(6));
-        vo_level->value(rkr->efx_Vocoder->getpar(5));
-}
-void RKRGUI::cb_vo_preset(Fl_Choice* o, void* v) {
-  ((RKRGUI*)(o->parent()->parent()->user_data()))->cb_vo_preset_i(o,v);
-}
-
-Fl_Menu_Item RKRGUI::menu_vo_preset[] = {
- {"Vocoder 1", 0,  0, 0, 0, (uchar)FL_NORMAL_LABEL, 0, 10, 0},
- {"Vocoder 2", 0,  0, 0, 0, (uchar)FL_NORMAL_LABEL, 0, 10, 0},
- {"Vocoder 3", 0,  0, 0, 0, (uchar)FL_NORMAL_LABEL, 0, 10, 0},
- {"Vocoder 4", 0,  0, 0, 0, (uchar)FL_NORMAL_LABEL, 0, 10, 0},
- {0,0,0,0,0,0,0,0,0}
-};
-
-void RKRGUI::cb_vo_WD_i(SliderW* o, void*) {
-  if(Fl::event_button()==3)
-        {
-         getMIDIControl(296);
-         return;
-        }
-        rkr->efx_Vocoder->changepar(0,(int)(o->value()+64));
-}
-void RKRGUI::cb_vo_WD(SliderW* o, void* v) {
-  ((RKRGUI*)(o->parent()->parent()->user_data()))->cb_vo_WD_i(o,v);
-}
-
-void RKRGUI::cb_vo_pan_i(SliderW* o, void*) {
-  if(Fl::event_button()==3)
-        {
-         getMIDIControl(297);
-         return;
-        }
-        rkr->efx_Vocoder->changepar(1,(int)(o->value()+64));
-}
-void RKRGUI::cb_vo_pan(SliderW* o, void* v) {
-  ((RKRGUI*)(o->parent()->parent()->user_data()))->cb_vo_pan_i(o,v);
-}
-
-void RKRGUI::cb_vo_input_i(SliderW* o, void*) {
-  if(Fl::event_button()==3)
-        {
-         getMIDIControl(298);
-         return;
-        }
-        rkr->efx_Vocoder->changepar(4,(int)o->value());
-}
-void RKRGUI::cb_vo_input(SliderW* o, void* v) {
-  ((RKRGUI*)(o->parent()->parent()->user_data()))->cb_vo_input_i(o,v);
-}
-
-void RKRGUI::cb_vo_mu_i(SliderW* o, void*) {
-  if(Fl::event_button()==3)
-        {
-         getMIDIControl(299);
-         return;
-        }
-        rkr->efx_Vocoder->changepar(2,(int)o->value());
-}
-void RKRGUI::cb_vo_mu(SliderW* o, void* v) {
-  ((RKRGUI*)(o->parent()->parent()->user_data()))->cb_vo_mu_i(o,v);
-}
-
-void RKRGUI::cb_vo_q_i(SliderW* o, void*) {
-  if(Fl::event_button()==3)
-        {
-         getMIDIControl(300);
-         return;
-        }
-        rkr->efx_Vocoder->changepar(3,(int)o->value());
-}
-void RKRGUI::cb_vo_q(SliderW* o, void* v) {
-  ((RKRGUI*)(o->parent()->parent()->user_data()))->cb_vo_q_i(o,v);
-}
-
-void RKRGUI::cb_vo_ring_i(SliderW* o, void*) {
-  if(Fl::event_button()==3)
-        {
-         getMIDIControl(301);
-         return;
-        }
-        rkr->efx_Vocoder->changepar(6,(int)o->value());
-}
-void RKRGUI::cb_vo_ring(SliderW* o, void* v) {
-  ((RKRGUI*)(o->parent()->parent()->user_data()))->cb_vo_ring_i(o,v);
-}
-
-void RKRGUI::cb_vo_level_i(SliderW* o, void*) {
-  if(Fl::event_button()==3)
-        {
-         getMIDIControl(302);
-         return;
-        }
-        rkr->efx_Vocoder->changepar(5,(int)o->value());
-}
-void RKRGUI::cb_vo_level(SliderW* o, void* v) {
-  ((RKRGUI*)(o->parent()->parent()->user_data()))->cb_vo_level_i(o,v);
 }
 
 void RKRGUI::cb_seq_activar_i(Fl_Light_Button* o, void*) {
@@ -5603,170 +5480,20 @@ Fl_Double_Window* RKRGUI::make_window() {
       SHELFBOOST->hide();
       SHELFBOOST->end();
     } // ShelfGui* SHELFBOOST
-    { VOCODER = new Fl_Group(161, 213, 158, 184);
+    { VOCODER = new VocoderGui(2, 413, 158, 184);
       VOCODER->box(FL_UP_BOX);
       VOCODER->color(FL_FOREGROUND_COLOR);
       VOCODER->selection_color(FL_FOREGROUND_COLOR);
+      VOCODER->labeltype(FL_NORMAL_LABEL);
       VOCODER->labelfont(1);
+      VOCODER->labelsize(14);
+      VOCODER->labelcolor(FL_FOREGROUND_COLOR);
       VOCODER->user_data((void*)(1));
       VOCODER->align(Fl_Align(96|FL_ALIGN_INSIDE));
+      VOCODER->when(FL_WHEN_RELEASE);
       VOCODER->hide();
-      { vo_activar = new Fl_Light_Button(167, 217, 34, 18, "On");
-        vo_activar->shortcut(0x35);
-        vo_activar->color((Fl_Color)62);
-        vo_activar->selection_color((Fl_Color)1);
-        vo_activar->labelsize(10);
-        vo_activar->callback((Fl_Callback*)cb_vo_activar, (void*)(2));
-        vo_activar->align(Fl_Align(68|FL_ALIGN_INSIDE));
-        vo_activar->when(FL_WHEN_CHANGED);
-      } // Fl_Light_Button* vo_activar
-      { vo_preset = new Fl_Choice(240, 217, 76, 18, "Preset");
-        vo_preset->down_box(FL_BORDER_BOX);
-        vo_preset->selection_color(FL_FOREGROUND_COLOR);
-        vo_preset->labelsize(10);
-        vo_preset->labelcolor(FL_BACKGROUND2_COLOR);
-        vo_preset->textsize(10);
-        vo_preset->textcolor(FL_BACKGROUND2_COLOR);
-        vo_preset->callback((Fl_Callback*)cb_vo_preset, (void*)(12035));
-        vo_preset->when(FL_WHEN_RELEASE_ALWAYS);
-        vo_preset->menu(menu_vo_preset);
-      } // Fl_Choice* vo_preset
-      { vo_WD = new SliderW(213, 243, 100, 10, "Wet/Dry");
-        vo_WD->type(5);
-        vo_WD->box(FL_FLAT_BOX);
-        vo_WD->color((Fl_Color)178);
-        vo_WD->selection_color((Fl_Color)62);
-        vo_WD->labeltype(FL_NORMAL_LABEL);
-        vo_WD->labelfont(0);
-        vo_WD->labelsize(10);
-        vo_WD->labelcolor(FL_BACKGROUND2_COLOR);
-        vo_WD->minimum(-64);
-        vo_WD->maximum(64);
-        vo_WD->step(1);
-        vo_WD->textcolor(FL_BACKGROUND2_COLOR);
-        vo_WD->callback((Fl_Callback*)cb_vo_WD);
-        vo_WD->align(Fl_Align(FL_ALIGN_LEFT));
-        vo_WD->when(FL_WHEN_CHANGED);
-      } // SliderW* vo_WD
-      { vo_pan = new SliderW(213, 263, 100, 10, "Pan");
-        vo_pan->type(5);
-        vo_pan->box(FL_FLAT_BOX);
-        vo_pan->color((Fl_Color)178);
-        vo_pan->selection_color((Fl_Color)62);
-        vo_pan->labeltype(FL_NORMAL_LABEL);
-        vo_pan->labelfont(0);
-        vo_pan->labelsize(10);
-        vo_pan->labelcolor(FL_BACKGROUND2_COLOR);
-        vo_pan->minimum(-64);
-        vo_pan->maximum(64);
-        vo_pan->step(1);
-        vo_pan->textcolor(FL_BACKGROUND2_COLOR);
-        vo_pan->callback((Fl_Callback*)cb_vo_pan);
-        vo_pan->align(Fl_Align(FL_ALIGN_LEFT));
-        vo_pan->when(FL_WHEN_CHANGED);
-      } // SliderW* vo_pan
-      { vo_input = new SliderW(213, 283, 100, 10, "Input");
-        vo_input->type(5);
-        vo_input->box(FL_FLAT_BOX);
-        vo_input->color((Fl_Color)178);
-        vo_input->selection_color((Fl_Color)62);
-        vo_input->labeltype(FL_NORMAL_LABEL);
-        vo_input->labelfont(0);
-        vo_input->labelsize(10);
-        vo_input->labelcolor(FL_BACKGROUND2_COLOR);
-        vo_input->maximum(127);
-        vo_input->step(1);
-        vo_input->textcolor(FL_BACKGROUND2_COLOR);
-        vo_input->callback((Fl_Callback*)cb_vo_input);
-        vo_input->align(Fl_Align(FL_ALIGN_LEFT));
-        vo_input->when(FL_WHEN_CHANGED);
-      } // SliderW* vo_input
-      { vo_mu = new SliderW(213, 303, 100, 10, "Muf.");
-        vo_mu->type(5);
-        vo_mu->box(FL_FLAT_BOX);
-        vo_mu->color((Fl_Color)178);
-        vo_mu->selection_color((Fl_Color)62);
-        vo_mu->labeltype(FL_NORMAL_LABEL);
-        vo_mu->labelfont(0);
-        vo_mu->labelsize(10);
-        vo_mu->labelcolor(FL_BACKGROUND2_COLOR);
-        vo_mu->minimum(1);
-        vo_mu->maximum(127);
-        vo_mu->step(1);
-        vo_mu->value(1);
-        vo_mu->textcolor(FL_BACKGROUND2_COLOR);
-        vo_mu->callback((Fl_Callback*)cb_vo_mu);
-        vo_mu->align(Fl_Align(FL_ALIGN_LEFT));
-        vo_mu->when(FL_WHEN_CHANGED);
-      } // SliderW* vo_mu
-      { vo_q = new SliderW(213, 323, 100, 10, "Q");
-        vo_q->type(5);
-        vo_q->box(FL_FLAT_BOX);
-        vo_q->color((Fl_Color)178);
-        vo_q->selection_color((Fl_Color)62);
-        vo_q->labeltype(FL_NORMAL_LABEL);
-        vo_q->labelfont(0);
-        vo_q->labelsize(10);
-        vo_q->labelcolor(FL_BACKGROUND2_COLOR);
-        vo_q->minimum(40);
-        vo_q->maximum(170);
-        vo_q->step(1);
-        vo_q->value(120);
-        vo_q->textcolor(FL_BACKGROUND2_COLOR);
-        vo_q->callback((Fl_Callback*)cb_vo_q);
-        vo_q->align(Fl_Align(FL_ALIGN_LEFT));
-        vo_q->when(FL_WHEN_CHANGED);
-      } // SliderW* vo_q
-      { vo_ring = new SliderW(213, 343, 100, 10, "Ring");
-        vo_ring->type(5);
-        vo_ring->box(FL_FLAT_BOX);
-        vo_ring->color((Fl_Color)178);
-        vo_ring->selection_color((Fl_Color)62);
-        vo_ring->labeltype(FL_NORMAL_LABEL);
-        vo_ring->labelfont(0);
-        vo_ring->labelsize(10);
-        vo_ring->labelcolor(FL_BACKGROUND2_COLOR);
-        vo_ring->maximum(127);
-        vo_ring->step(1);
-        vo_ring->textcolor(FL_BACKGROUND2_COLOR);
-        vo_ring->callback((Fl_Callback*)cb_vo_ring);
-        vo_ring->align(Fl_Align(FL_ALIGN_LEFT));
-        vo_ring->when(FL_WHEN_CHANGED);
-      } // SliderW* vo_ring
-      { vo_level = new SliderW(213, 363, 100, 10, "Level");
-        vo_level->type(5);
-        vo_level->box(FL_FLAT_BOX);
-        vo_level->color((Fl_Color)178);
-        vo_level->selection_color((Fl_Color)62);
-        vo_level->labeltype(FL_NORMAL_LABEL);
-        vo_level->labelfont(0);
-        vo_level->labelsize(10);
-        vo_level->labelcolor(FL_BACKGROUND2_COLOR);
-        vo_level->maximum(127);
-        vo_level->step(1);
-        vo_level->textcolor(FL_BACKGROUND2_COLOR);
-        vo_level->callback((Fl_Callback*)cb_vo_level);
-        vo_level->align(Fl_Align(FL_ALIGN_LEFT));
-        vo_level->when(FL_WHEN_CHANGED);
-      } // SliderW* vo_level
-      { vu_vu = new NewVum(168, 268, 11, 122);
-        vu_vu->type(2);
-        vu_vu->box(FL_NO_BOX);
-        vu_vu->color((Fl_Color)178);
-        vu_vu->selection_color((Fl_Color)90);
-        vu_vu->labeltype(FL_NORMAL_LABEL);
-        vu_vu->labelfont(0);
-        vu_vu->labelsize(14);
-        vu_vu->labelcolor(FL_FOREGROUND_COLOR);
-        vu_vu->minimum(15);
-        vu_vu->maximum(-48);
-        vu_vu->step(1);
-        vu_vu->value(-48);
-        vu_vu->align(Fl_Align(FL_ALIGN_CLIP|FL_ALIGN_INSIDE));
-        vu_vu->when(FL_WHEN_NEVER);
-      } // NewVum* vu_vu
       VOCODER->end();
-    } // Fl_Group* VOCODER
+    } // VocoderGui* VOCODER
     { SUSTAINER = new SustainGui(320, 212, 158, 184);
       SUSTAINER->box(FL_UP_BOX);
       SUSTAINER->color(FL_FOREGROUND_COLOR);
@@ -10735,8 +10462,8 @@ void RKRGUI::Put_Loaded() {
        break;
        
        case 35://Vocoder
-       vo_activar->value(rkr->Vocoder_Bypass);
-       vo_preset->do_callback(vo_preset,1);
+       VOCODER->vo_activar->value(rkr->Vocoder_Bypass);
+       VOCODER->vo_preset->do_callback(VOCODER->vo_preset,1);
        break;
   
        case 36://Sustainer
@@ -11488,7 +11215,7 @@ void RKRGUI::reordena() {
   
      case 35:
          VOCODER->position(x[i],y[i]);
-         vo_activar->shortcut(s[i]);
+         VOCODER->vo_activar->shortcut(s[i]);
          if(!rkr->deachide)VOCODER->show();
          if(rkr->Vocoder_Bypass)
          {
@@ -13295,32 +13022,32 @@ void RKRGUI::ActMIDI() {
        SHELFBOOST->shelf_q1->redraw();     
        break;
        case 296:
-       vo_WD->value(rkr->efx_Vocoder->getpar(0)-64); 
-       vo_WD->redraw();
+       VOCODER->vo_WD->value(rkr->efx_Vocoder->getpar(0)-64); 
+       VOCODER->vo_WD->redraw();
        break;
        case 297:
-       vo_pan->value(rkr->efx_Vocoder->getpar(1)-64); 
-       vo_pan->redraw();
+       VOCODER->vo_pan->value(rkr->efx_Vocoder->getpar(1)-64); 
+       VOCODER->vo_pan->redraw();
        break;
        case 298:
-       vo_input->value(rkr->efx_Vocoder->getpar(4)); 
-       vo_input->redraw();
+       VOCODER->vo_input->value(rkr->efx_Vocoder->getpar(4)); 
+       VOCODER->vo_input->redraw();
        break;
        case 299:
-       vo_mu->value(rkr->efx_Vocoder->getpar(2)); 
-       vo_mu->redraw();
+       VOCODER->vo_mu->value(rkr->efx_Vocoder->getpar(2)); 
+       VOCODER->vo_mu->redraw();
        break;
        case 300:
-       vo_q->value(rkr->efx_Vocoder->getpar(3)); 
-       vo_q->redraw();
+       VOCODER->vo_q->value(rkr->efx_Vocoder->getpar(3)); 
+       VOCODER->vo_q->redraw();
        break;
        case 301:
-       vo_ring->value(rkr->efx_Vocoder->getpar(6)); 
-       vo_ring->redraw();
+       VOCODER->vo_ring->value(rkr->efx_Vocoder->getpar(6)); 
+       VOCODER->vo_ring->redraw();
        break;
        case 302:
-       vo_level->value(rkr->efx_Vocoder->getpar(5)); 
-       vo_level->redraw();
+       VOCODER->vo_level->value(rkr->efx_Vocoder->getpar(5)); 
+       VOCODER->vo_level->redraw();
        break;
        case 303:
        RBECHO->rbecho_WD->value(rkr->efx_RBEcho->getpar(0)-64);
@@ -13894,8 +13621,8 @@ void RKRGUI::ActOnOff() {
     SHELFBOOST->shelf_activar->do_callback();
     break;
     case 35:
-    vo_activar->value(rkr->Vocoder_Bypass);
-    vo_activar->do_callback();
+    VOCODER->vo_activar->value(rkr->Vocoder_Bypass);
+    VOCODER->vo_activar->do_callback();
     break;
     case 36:
     SUSTAINER->sus_activar->value(rkr->Sustainer_Bypass);
@@ -15713,7 +15440,7 @@ void RKRGUI::RandomPreset() {
        
        case 35://Vocoder
        if (i<numEff)rkr->Vocoder_Bypass=1; else rkr->Vocoder_Bypass=0;
-       vo_activar->value(rkr->Vocoder_Bypass);
+       VOCODER->vo_activar->value(rkr->Vocoder_Bypass);
        break;
   
        case 36://Sustainer
--- src/UI/rakarrack.h
+++ src/UI/rakarrack.h
@@ -59,6 +59,7 @@ class RyanwahGui; // forward declaration
 class RbechoGui; // forward declaration
 class CoilGui; // forward declaration
 class ShelfGui; // forward declaration
+class VocoderGui; // forward declaration
 class SustainGui; // forward declaration
 #include <FL/Fl_Double_Window.H>
 #include <FL/Fl_Box.H>
@@ -100,10 +101,11 @@ class SustainGui; // forward declaration
 #include "rbecho_gui.h"
 #include "coil_gui.h"
 #include "shelf_gui.h"
+#include "vocoder_gui.h"
+#include "sustain_gui.h"
 #include <FL/Fl_Group.H>
 #include <FL/Fl_Light_Button.H>
 #include <FL/Fl_Choice.H>
-#include "sustain_gui.h"
 #include <FL/Fl_Check_Button.H>
 #include <FL/Fl_Counter.H>
 #include <FL/Fl_Value_Input.H>
@@ -298,54 +300,7 @@ public:
   RbechoGui *RBECHO;
   CoilGui *COILCRAFTER;
   ShelfGui *SHELFBOOST;
-  Fl_Group *VOCODER;
-  Fl_Light_Button *vo_activar;
-private:
-  inline void cb_vo_activar_i(Fl_Light_Button*, void*);
-  static void cb_vo_activar(Fl_Light_Button*, void*);
-public:
-  Fl_Choice *vo_preset;
-private:
-  inline void cb_vo_preset_i(Fl_Choice*, void*);
-  static void cb_vo_preset(Fl_Choice*, void*);
-  static Fl_Menu_Item menu_vo_preset[];
-public:
-  SliderW *vo_WD;
-private:
-  inline void cb_vo_WD_i(SliderW*, void*);
-  static void cb_vo_WD(SliderW*, void*);
-public:
-  SliderW *vo_pan;
-private:
-  inline void cb_vo_pan_i(SliderW*, void*);
-  static void cb_vo_pan(SliderW*, void*);
-public:
-  SliderW *vo_input;
-private:
-  inline void cb_vo_input_i(SliderW*, void*);
-  static void cb_vo_input(SliderW*, void*);
-public:
-  SliderW *vo_mu;
-private:
-  inline void cb_vo_mu_i(SliderW*, void*);
-  static void cb_vo_mu(SliderW*, void*);
-public:
-  SliderW *vo_q;
-private:
-  inline void cb_vo_q_i(SliderW*, void*);
-  static void cb_vo_q(SliderW*, void*);
-public:
-  SliderW *vo_ring;
-private:
-  inline void cb_vo_ring_i(SliderW*, void*);
-  static void cb_vo_ring(SliderW*, void*);
-public:
-  SliderW *vo_level;
-private:
-  inline void cb_vo_level_i(SliderW*, void*);
-  static void cb_vo_level(SliderW*, void*);
-public:
-  NewVum *vu_vu;
+  VocoderGui *VOCODER;
   SustainGui *SUSTAINER;
   Fl_Group *SEQUENCE;
   Fl_Light_Button *seq_activar;
