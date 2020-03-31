e0bebc35ccfc08ab54cc9ab70aa16b33567af3dd
stazed <stazed10@KudoMapson.com>
2020-03-31 09:36:05 -0500

    Add GNU public license to inputW.h, inputW.cxx. Additional notes.

--- src/UI/inputW.h
+++ src/UI/inputW.h
@@ -1,8 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+  inputW.h
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License (version 2) for more details.
+
+  You should have received a copy of the GNU General Public License (version 2)
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+*/
 
 /* 
  * File:   inputW.h
