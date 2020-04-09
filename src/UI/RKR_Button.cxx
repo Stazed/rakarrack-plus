f0b6bc8a0d2d7685838ed86ec1b0e0b0af879194
stazed <stazed@manjodesk.com>
2020-04-08 22:12:35 -0500

    Add RKR_Button class for label resizing. Implement with the preset "New" button.

---
+++ src/UI/RKR_Button.cxx
@@ -0,0 +1,63 @@
+/*
+  RKR_Buttom.cxx
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License (version 2) for more details.
+
+  You should have received a copy of the GNU General Public License (version 2)
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+*/
+
+/* 
+ * File:   RKR_Button.cxx
+ * Author: sspresto
+ * 
+ * Created on April 8, 2020, 9:46 PM
+ */
+
+#include "RKR_Button.h"
+#include "../global.h"
+
+RKR_Button::RKR_Button(int X, int Y, int W, int H, const char *label) : Fl_Button(X, Y, W, H, label)
+{
+    m_need_font_update = true;
+    m_start_height = H;
+    m_start_font_offset = 0;
+    this->user_data((void*)(BUTTON_USER_DATA));
+}
+
+void RKR_Button::draw()
+{
+    if(m_need_font_update)
+    {
+        m_need_font_update = false;
+        font_resize(x(), y(), w(), h());
+    }
+
+    Fl_Button::draw();
+}
+
+void RKR_Button::font_resize(int X, int Y, int W, int H)
+{
+    float H_ratio = (float) H / m_start_height;
+    int font_size = g_value_font_size + m_start_font_offset;
+    int H_size = (float) (font_size * H_ratio);
+    
+    labelsize(H_size);
+}
+
+void RKR_Button::resize(int X, int Y, int W, int H)
+{
+    /* Resize the text and labels */
+    font_resize(X, Y, W, H);
+
+    Fl_Button::resize(X, Y, W, H);
+}
---
+++ src/UI/RKR_Button.h
@@ -0,0 +1,54 @@
+/*
+  RKR_Button.h
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License (version 2) for more details.
+
+  You should have received a copy of the GNU General Public License (version 2)
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+*/
+
+/* 
+ * File:   RKR_Button.h
+ * Author: sspresto
+ *
+ * Created on April 8, 2020, 9:46 PM
+ */
+
+#ifndef RKR_BUTTON_H
+#define RKR_BUTTON_H
+
+#include <FL/Fl.H>
+#include <FL/Fl_Button.H>
+#include <FL/fl_draw.H>
+
+#define BUTTON_USER_DATA   77
+
+class RKR_Button : public Fl_Button
+{
+public:
+    RKR_Button(int X, int Y, int W, int H, const char *label=0);
+    void draw();
+    void resize(int,int,int,int);
+    void set_font_adjustment(){m_need_font_update = true;}
+    int m_start_height;
+    int m_start_font_offset;
+private:
+    
+    bool m_need_font_update;
+    void font_resize(int,int,int,int);
+
+private:
+
+};
+
+#endif /* RKR_BUTTON_H */
+
