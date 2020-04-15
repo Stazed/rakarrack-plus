dfe1463789a2f8c5dcc63947943eebdb6ff8a846
stazed <stazed10@KudoMapson.com>
2020-04-15 04:33:10 -0500

    Add RKR_File_Input class. Apply to settingswindow.

---
+++ src/UI/RKR_File_Input.cxx
@@ -0,0 +1,76 @@
+/*
+  RKR_File_Input.cxx
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License (version 2) for more details.
+
+  You should have received a copy of the GNU General Public License (version 2)
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+*/
+
+/* 
+ * File:   RKR_File_Input.cxx
+ * Author: sspresto
+ * 
+ * Created on April 15, 2020, 4:11 AM
+ */
+
+#include "RKR_File_Input.h"
+#include "../global.h"
+
+RKR_File_Input::RKR_File_Input(int X, int Y, int W, int H, const char *label) : Fl_File_Input(X, Y, W, H, label)
+{
+    m_previous_font_size = g_default_font_size;
+    m_start_width = W;
+    m_start_height = H;
+    m_start_label_offset = 0;
+    m_start_text_offset = 0;
+}
+
+void RKR_File_Input::draw()
+{
+    /* To update the font size if user changes the value in settings */
+    if(g_default_font_size != m_previous_font_size)
+    {
+        m_previous_font_size = g_default_font_size;
+        font_resize(w(), h());
+    }
+
+    Fl_File_Input::draw();
+}
+
+void RKR_File_Input::font_resize(int W, int H)
+{
+    float W_ratio = (float) W / m_start_width;
+    float H_ratio = (float) H / m_start_height;
+    float resize_ratio = (W_ratio < H_ratio) ? W_ratio : H_ratio;
+    
+    int font_size = g_default_font_size + m_start_label_offset;
+    int adjusted_label_size = (float) (font_size * resize_ratio);
+    
+    labelsize(adjusted_label_size);
+    
+    int text_font_size = g_default_font_size + m_start_text_offset;
+    int adjusted_text_size = (float) (text_font_size * resize_ratio);
+    
+    textsize(adjusted_text_size);
+}
+
+void RKR_File_Input::resize(int X, int Y, int W, int H)
+{
+    /* Resize the text and labels */
+    font_resize(W, H);
+
+    Fl_File_Input::resize(X, Y, W, H);
+}
+
+
+
---
+++ src/UI/RKR_File_Input.h
@@ -0,0 +1,49 @@
+/*
+  RKR_File_Input.h
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License (version 2) for more details.
+
+  You should have received a copy of the GNU General Public License (version 2)
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+*/
+
+/* 
+ * File:   RKR_File_Input.h
+ * Author: sspresto
+ *
+ * Created on April 15, 2020, 4:11 AM
+ */
+
+#ifndef RKR_FILE_INPUT_H
+#define RKR_FILE_INPUT_H
+
+#include <FL/Fl.H>
+#include <FL/Fl_File_Input.H>
+
+class RKR_File_Input : public Fl_File_Input
+{
+public:
+    RKR_File_Input(int X, int Y, int W, int H, const char *label=0);
+    void draw();
+    void resize(int,int,int,int);
+    int m_start_label_offset;
+    int m_start_text_offset;
+private:
+
+    int m_start_height;
+    int m_start_width;
+    int m_previous_font_size;
+    void font_resize(int,int);
+};
+
+#endif /* RKR_FILE_INPUT_H */
+
