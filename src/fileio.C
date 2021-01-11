6b848b9f644b42f546717e29985313d17961b245
stazed <stazed10@KudoMapson.com>
2021-01-11 07:13:35 -0600

    Fix incorrect memset().

--- src/fileio.C
+++ src/fileio.C
@@ -373,7 +373,7 @@ void RKR::putbuf(char *buf, int j)
     case 29:
         //Convolotron
         memset(efx_Convol->Filename, 0, sizeof (efx_Convol->Filename));
-        memset(cfilename, 0, sizeof (*cfilename));
+        memset(cfilename, 0, sizeof (char) * 128);
         sscanf(buf, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%s\n",
                &lv[30][0], &lv[30][1], &lv[30][2], &lv[30][3], &lv[30][4],
                &lv[30][5], &lv[30][6], &lv[30][7], &lv[30][8], &lv[30][9],
@@ -458,7 +458,7 @@ void RKR::putbuf(char *buf, int j)
     case 40:
         //Reverbtron
         memset(efx_Reverbtron->Filename, 0, sizeof (efx_Reverbtron->Filename));
-        memset(cfilename, 0, sizeof (*cfilename));
+        memset(cfilename, 0, sizeof (char) * 128);
         sscanf(buf, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%s\n",
                &lv[41][0], &lv[41][1], &lv[41][2], &lv[41][3], &lv[41][4],
                &lv[41][5], &lv[41][6], &lv[41][7], &lv[41][8], &lv[41][9],
@@ -471,7 +471,7 @@ void RKR::putbuf(char *buf, int j)
     case 41:
         //Echotron
         memset(efx_Echotron->Filename, 0, sizeof (efx_Echotron->Filename));
-        memset(cfilename, 0, sizeof (*cfilename));
+        memset(cfilename, 0, sizeof (char) * 128);
         sscanf(buf, "%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%s\n",
                &lv[42][0], &lv[42][1], &lv[42][2], &lv[42][3], &lv[42][4],
                &lv[42][5], &lv[42][6], &lv[42][7], &lv[42][8], &lv[42][9],
@@ -2002,11 +2002,11 @@ RKR::New()
         active[j] = 0;
     }
 
-    memset(Preset_Name, 0, sizeof (*Preset_Name));
+    memset(Preset_Name, 0, sizeof (char) * 64);
     memset(efx_Convol->Filename, 0, sizeof (efx_Convol->Filename));
     memset(efx_Reverbtron->Filename, 0, sizeof (efx_Reverbtron->Filename));
     memset(efx_Echotron->Filename, 0, sizeof (efx_Echotron->Filename));
-    memset(Author, 0, sizeof (*Author));
+    memset(Author, 0, sizeof (char) * 64);
     strcpy(Author, UserRealName);
     Input_Gain = .5f;
     Master_Volume = .5f;
@@ -2118,9 +2118,9 @@ RKR::New_Bank()
 void
 RKR::Bank_to_Preset(int i)
 {
-    memset(Preset_Name, 0, sizeof (*Preset_Name));
+    memset(Preset_Name, 0, sizeof (char) * 64);
     strcpy(Preset_Name, Bank[i].Preset_Name);
-    memset(Author, 0, sizeof (*Author));
+    memset(Author, 0, sizeof (char) * 64);
     strcpy(Author, Bank[i].Author);
     memset(efx_Convol->Filename, 0, sizeof (efx_Convol->Filename));
     strcpy(efx_Convol->Filename, Bank[i].ConvoFiname);
