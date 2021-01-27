38f24f950f4487323160bf1038f24ec90a2b84ee
stazed <stazed10@KudoMapson.com>
2021-01-27 06:08:49 -0600

    Initial re-factor of Effect class.

--- src/Effect.h
+++ src/Effect.h
@@ -24,39 +24,54 @@
 #define EFFECT_H
 
 #include "global.h"
-#include "FilterParams.h"
+#include <vector>
 
 
 class Effect
 {
 public:
 
-    virtual ~ Effect () {
-    };
-    virtual void setpreset (unsigned char npreset) {
-    };
-    virtual void changepar (int npar, unsigned char value) {
-    };
-    virtual unsigned char getpar (int npar) {
-        return (0);
-    };
-    virtual void out (float * smpsl, float * smpsr) {
-    };
-    virtual void cleanup () {
-    };
-    virtual float getfreqresponse (float freq) {
-        return (0);
-    };				//this is only used for EQ (for user interface)
+    Effect (double sample_rate, uint32_t intermediate_bufsize) :
+        outvolume(0.5f), Ppreset(), Fpre(NULL) {};
 
-    unsigned char Ppreset;
+    // Distortion, Overdrive, Derelict, DistBand, StompBox, Convolotron, Reverbtron
+    Effect (int, int, int, double, uint32_t) :
+        outvolume(0.5f), Ppreset(), Fpre(NULL) {};
 
-    float *efxoutl;
-    float *efxoutr;
+    // Harmonizer, Sequence, Shifter, StereoHarm
+    Effect (long int Quality, int DS, int uq, int dq, double sample_rate,
+        uint32_t intermediate_bufsize) :
+        outvolume(0.5f), Ppreset(), Fpre(NULL) {};
 
-    float outvolume;		//this is the volume of effect and is public because need it in system effect. The out volume of such effects are always 1.0, so this setting tells me how is the volume to the Master Output only.
+    // Looper
+    Effect (float size, double samplerate, uint32_t intermediate_bufsize) :
+        outvolume(0.5f), Ppreset(), Fpre(NULL) {};
 
+    // Vocoder
+    Effect (float *auxresampled_, int bands, int DS, int uq, int dq,
+        double sample_rate, uint32_t intermediate_bufsize) :
+        outvolume(0.5f), Ppreset(), Fpre(NULL) {};
 
-    FilterParams *filterpars;
+    virtual ~ Effect () {};
+    
+    virtual void setpreset (int npreset) {};
+    virtual void changepar (int npar, int value) {};
+    virtual int getpar (int npar) {return (0);};
+    virtual void out (float * smpsl, float * smpsr) {};
+    virtual void cleanup () {};
+    
+    virtual std::vector<int> save_parameters() {};
+    virtual void reset_parameters(std::vector<int> parameters) {};
+
+#ifdef LV2_SUPPORT
+    virtual void lv2_update_params(uint32_t period) {};
+#endif // LV2
+
+    // The volume of the effect and is public.
+    float outvolume;
+    int Ppreset;
+    class FPreset *Fpre;
+
 };
 
 #endif
