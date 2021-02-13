7a4aff1c992bf07e9caaebd5add40003562cfdd3
stazed <stazed10@KudoMapson.com>
2021-02-12 19:32:36 -0600

    Add RKR_GUI_Effect class. Base gui class for rack effects.

---
+++ src/UI/RKR_GUI_Effect.cxx
@@ -0,0 +1,30 @@
+/*
+  RKR_GUI_Effect.cxx
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License (version 2) for more details.
+
+  You should have received a copy of the GNU General Public License (version 2)
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+*/
+
+/* 
+ * File:   RKR_GUI_Effect.cxx
+ * Author: sspresto
+ * 
+ * Created on February 12, 2021, 11:21 AM
+ */
+
+#include "RKR_GUI_Effect.h"
+
+RKR_Gui_Effect::RKR_Gui_Effect(int X, int Y, int W, int H, const char *L)
+  : Fl_Group(X, Y, W, H, L)
+{}
---
+++ src/UI/RKR_GUI_Effect.h
@@ -0,0 +1,42 @@
+/*
+  RKR_GUI_Effect.h
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License (version 2) for more details.
+
+  You should have received a copy of the GNU General Public License (version 2)
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+*/
+
+/* 
+ * File:   RKR_GUI_Effect.h
+ * Author: sspresto
+ *
+ * Created on February 12, 2021, 11:21 AM
+ */
+
+#ifndef RKR_GUI_EFFECT_H
+#define RKR_GUI_EFFECT_H
+
+#include "RKR_Light_Button.h"
+#include "RKR_Choice.h"
+
+class RKR_Gui_Effect : public Fl_Group
+{
+public:
+    RKR_Gui_Effect(int X, int Y, int W, int H, const char *L);
+
+    RKR_Light_Button *activate_effect;
+    RKR_Choice *preset_choice;
+
+};
+
+#endif /* RKR_GUI_EFFECT_H */
