d9b28a83b05a08313034c33c42b432dcea93da51
Stazed <stazed@manjo-mapson.com>
2022-01-20 19:22:20 -0600

    Add echotron delay file window archive.

---
+++ src/UI/RKR_Float_Input.cxx
@@ -0,0 +1,93 @@
+/*
+  RKR_Float_Input.cxx
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License (version 2) for more details.
+
+  You should have received a copy of the GNU General Public License (version 2)
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+*/
+
+/* 
+ * File:   RKR_Float_Input.cxx
+ * Author: sspresto
+ * 
+ * Created on May 15, 2020, 11:45 AM
+ */
+
+#include "RKR_Float_Input.h"
+#include "../global.h"
+
+RKR_Float_Input::RKR_Float_Input(int X, int Y, int W, int H, const char *label) :
+    Fl_Float_Input(X, Y, W, H, label),
+    m_label_offset(0),      // C_DEFAULT_FONT_SIZE
+    m_text_offset(0),       // C_DEFAULT_FONT_SIZE
+    m_start_width(W),
+    m_start_height(H),
+    m_previous_font_size(global_font_size),
+    m_max_value(0.0),
+    m_min_value(0.0)
+{
+}
+
+void RKR_Float_Input::draw()
+{
+    /* To update the font size if user changes the value in settings */
+    if(global_font_size != m_previous_font_size)
+    {
+        m_previous_font_size = global_font_size;
+        font_resize(w(), h());
+    }
+
+    Fl_Float_Input::draw();
+}
+
+void RKR_Float_Input::font_resize(int W, int H)
+{
+    float W_ratio = (float) W / m_start_width;
+    float H_ratio = (float) H / m_start_height;
+    float resize_ratio = (W_ratio < H_ratio) ? W_ratio : H_ratio;
+    
+    int font_size = global_font_size + m_label_offset;
+    int adjusted_label_size = (float) (font_size * resize_ratio);
+    
+    labelsize(adjusted_label_size);
+    
+    int text_font_size = global_font_size + m_text_offset;
+    int adjusted_text_size = (float) (text_font_size * resize_ratio);
+    
+    textsize(adjusted_text_size);
+}
+
+void RKR_Float_Input::resize(int X, int Y, int W, int H)
+{
+    /* Resize the text and labels */
+    font_resize(W, H);
+
+    Fl_Float_Input::resize(X, Y, W, H);
+}
+
+int RKR_Float_Input::handle(int event)
+{
+    int ret = Fl_Input::handle(event);
+    
+    if(strcmp(value(), ""))
+    {
+        return ret; // Not blank - we have a value
+    }
+    else
+    {
+        value("0"); // User cleared the field - this is minimum
+    }
+    
+    return ret;
+}
+
---
+++ src/UI/RKR_Float_Input.h
@@ -0,0 +1,56 @@
+/*
+  RKR_Float_Input.h
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of version 2 of the GNU General Public License
+  as published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License (version 2) for more details.
+
+  You should have received a copy of the GNU General Public License (version 2)
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+*/
+
+/* 
+ * File:   RKR_Float_Input.h
+ * Author: sspresto
+ *
+ * Created on May 15, 2020, 11:45 AM
+ */
+
+#ifndef RKR_FLOAT_INPUT_H
+#define RKR_FLOAT_INPUT_H
+
+#include <FL/Fl_Float_Input.H>
+
+class RKR_Float_Input  : public Fl_Float_Input
+{
+public:
+    RKR_Float_Input(int X, int Y, int W, int H, const char *label=0);
+    void draw();
+    void resize(int,int,int,int);
+    int handle(int);
+    void set_label_offset(int offset){m_label_offset = offset;};
+    void set_text_offset(int offset){m_text_offset = offset;};
+    void set_maximum_value(double val){m_max_value = val;};
+    void set_minimum_value(double val){m_min_value = val;};
+
+private:
+
+    int m_label_offset;
+    int m_text_offset;
+    int m_start_width;
+    int m_start_height;
+    int m_previous_font_size;
+    double m_max_value;
+    double m_min_value;
+    void font_resize(int,int);
+};
+
+#endif /* RKR_FLOAT_INPUT_H */
+
