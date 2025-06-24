ff6e9e3d402f7ac841f4daca72fb7f49a89f2f6d
Stazed <stazed@cachyOS-mapson.com>
2025-06-24 08:19:30 -0500

    Fix Reverbtron LV2 period update.

--- src/FX/Reverbtron.C
+++ src/FX/Reverbtron.C
@@ -194,6 +194,9 @@ Reverbtron::lv2_update_params(uint32_t period)
         adjust(DS_state, fSAMPLE_RATE);
         clear_initialize();
         initialize();
+#ifdef RKR_PLUS_LV2
+        setfile(Filenum);
+#endif
         setlpf(Plpf);
         sethidamp(Phidamp);
     }
